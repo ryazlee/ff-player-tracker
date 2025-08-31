387f221e686b90978f8d64a7a9453a0dfbc91b44
Ryan Lee <ryan.j.lee99@gmail.com>
2025-08-31 14:30:48 -0700

    Add helper

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,10 +5,18 @@ import { RedditPostsList } from "./RedditPostsList";
 export const Main = () => {
 	const [players, setPlayers] = useState<string[]>([]);
 
+	const helpMessage =
+		"This is a tool for you to use to keep track of you fantasy football players!  Just enter the names of the player in the input and it will fetch the most recent Reddit posts about them.";
+
 	return (
 		<>
 			<h1>Fantasy Football Player Tracker</h1>
 			<UserInputForm players={players} setPlayers={setPlayers} />
+			{players.length == 0 && (
+				<p style={{ fontStyle: "italic", color: "gray" }}>
+					{helpMessage}
+				</p>
+			)}
 			{players.length > 0 && <RedditPostsList players={players} />}
 		</>
 	);
